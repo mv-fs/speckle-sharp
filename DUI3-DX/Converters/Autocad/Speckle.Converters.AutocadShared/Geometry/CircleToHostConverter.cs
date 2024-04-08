7b097ba019c01a17967335a784fe813c3463c4f6
Claire Kuang <kuang.claire@gmail.com>
2024-04-08 11:44:11 +0100

    refactors circle to host conversion

--- DUI3-DX/Converters/Autocad/Speckle.Converters.AutocadShared/Geometry/CircleToHostConverter.cs
+++ DUI3-DX/Converters/Autocad/Speckle.Converters.AutocadShared/Geometry/CircleToHostConverter.cs
@@ -1,5 +1,7 @@
-﻿using Autodesk.AutoCAD.DatabaseServices;
+using System;
+using Autodesk.AutoCAD.DatabaseServices;
 using Autodesk.AutoCAD.Geometry;
+using Objects.Geometry;
 using Speckle.Converters.Common;
 using Speckle.Converters.Common.Objects;
 using Speckle.Core.Kits;
@@ -10,56 +12,39 @@ namespace Speckle.Converters.Autocad.Geometry;
 [NameAndRankValue(nameof(SOG.Circle), NameAndRankValueAttribute.SPECKLE_DEFAULT_RANK)]
 public class DBCircleToHostConverter : ISpeckleObjectToHostConversion, IRawConversion<SOG.Circle, ADB.Circle>
 {
-  private readonly IRawConversion<SOG.Circle, AG.CircularArc3d> _circleConverter;
+  private readonly IRawConversion<SOG.Point, AG.Point3d> _pointConverter;
+  private readonly IRawConversion<SOG.Vector, AG.Vector3d> _vectorConverter;
   private readonly IConversionContextStack<Document, UnitsValue> _contextStack;
 
   public DBCircleToHostConverter(
-    IRawConversion<SOG.Circle, AG.CircularArc3d> circleConverter,
+    IRawConversion<SOG.Point, AG.Point3d> pointConverter,
+    IRawConversion<Vector, Vector3d> vectorConverter,
     IConversionContextStack<Document, UnitsValue> contextStack
   )
   {
-    _circleConverter = circleConverter;
+    _pointConverter = pointConverter;
+    _vectorConverter = vectorConverter;
     _contextStack = contextStack;
   }
 
   public object Convert(Base target) => RawConvert((SOG.Circle)target);
 
-  public Circle RawConvert(SOG.Circle target)
+  public ADB.Circle RawConvert(SOG.Circle target)
   {
-    CircularArc3d circle = _circleConverter.RawConvert(target);
-    return new(circle.Center, circle.Normal, circle.Radius);
-  }
-}
+    AG.Vector3d normal = _vectorConverter.RawConvert(target.plane.normal);
+    AG.Point3d origin = _pointConverter.RawConvert(target.plane.origin);
 
-public class CircleToHostConverter : IRawConversion<SOG.Circle, CircularArc3d>
-{
-  private readonly IConversionContextStack<Document, UnitsValue> _contextStack;
-  private readonly IRawConversion<SOG.Point, AG.Point3d> _pointConverter;
-  private readonly IRawConversion<SOG.Vector, AG.Vector3d> _vectorConverter;
-
-  public CircleToHostConverter(
-    IConversionContextStack<Document, UnitsValue> contextStack,
-    IRawConversion<SOG.Point, AG.Point3d> pointConverter,
-    IRawConversion<SOG.Vector, AG.Vector3d> vectorConverter
-  )
-  {
-    _contextStack = contextStack;
-    _pointConverter = pointConverter;
-    _vectorConverter = vectorConverter;
-  }
-
-  public CircularArc3d RawConvert(SOG.Circle target)
-  {
-    Point3d center = _pointConverter.RawConvert(target.plane.origin);
-    Vector3d normal = _vectorConverter.RawConvert(target.plane.normal);
     double f = Units.GetConversionFactor(target.units, _contextStack.Current.SpeckleUnits);
-
-    CircularArc3d arc = new(center, normal, 0);
+    double radius;
     if (target.radius is double targetRadius)
     {
-      double radius = targetRadius * f;
-      arc.Radius = radius;
+      radius = f * targetRadius;
     }
-    return arc;
+    else
+    {
+      radius = f * Math.Sqrt(target.area / Math.PI);
+    }
+
+    return new(origin, normal, radius);
   }
 }
